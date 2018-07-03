43587e299e972db09a33184edbd13dd338143a53
Bailey Miller <bmiller@susquehannasoftware.com>
2018-07-03 05:25:53 -0400

    Client can now listen for incoming client updates as well can request client updates.

--- Client/src/app/ComponentPreviews/cpu-usage-preview/cpu-usage-preview.component.ts
+++ Client/src/app/ComponentPreviews/cpu-usage-preview/cpu-usage-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StatisticsService } from '../../Services/statistics.service';
 
 @Component({
   selector: 'cpu-usage-preview',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CpuUsagePreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public statsApi: StatisticsService) { }
 
   ngOnInit() {
   }
