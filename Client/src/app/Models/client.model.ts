4df92001fdb4306dd7537d530f550d8210142952
Bailey Miller <bmiller@susquehannasoftware.com>
2018-07-05 19:47:21 -0400

    Added group property to allow for builds to complete

--- Client/src/app/Models/client.model.ts
+++ Client/src/app/Models/client.model.ts
@@ -1,4 +1,4 @@
 export class Client
 { 
-    constructor(public connectionId: string, public osFamily: string, public deviceFamily: string) { }
+    constructor(public connectionId: string, public osFamily: string, public deviceFamily: string, public group: any) { }
 }
